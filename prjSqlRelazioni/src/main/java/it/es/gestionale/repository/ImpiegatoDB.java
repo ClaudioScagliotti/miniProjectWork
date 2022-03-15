02461e904a41a73c947e5b58ca9c7f7a30400619
EmanueleKumanaku <EmanueleKumanaku@users.noreply.github.com>
2022-03-15 18:57:47 +0100

    impiegatoDB

--- prjSqlRelazioni/src/main/java/it/es/gestionale/repository/ImpiegatoDB.java
+++ prjSqlRelazioni/src/main/java/it/es/gestionale/repository/ImpiegatoDB.java
@@ -1,5 +1,7 @@
 package it.es.gestionale.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,6 +9,11 @@ import it.es.gestionale.model.ImpiegatoEntity;
 
 @Repository
 public interface ImpiegatoDB extends JpaRepository<ImpiegatoEntity, Integer>{
-	
+
+    public List<ImpiegatoEntity> findAllByOrderByNome();
+
+    public List<ImpiegatoEntity> findAllByOrderByCognome();
+
+    public List<ImpiegatoEntity> findByStipendioBetween(double min,double max);
 }
 
